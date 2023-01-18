964ab93cd966de4aad54ae3590c08798727a7d17
nijamud <nijam.mohamedabuback@sainsburys.co.uk>
2023-01-18 10:10:05 +0000

    Add filter criteria

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,5 +18,15 @@ resource "aws_lambda_event_source_mapping" "example" {
   event_source_arn  = aws_dynamodb_table.dynamodb_table.stream_arn
   function_name     = aws_lambda_function.lambda_function.arn
   starting_position = "LATEST"
-  batch_size = "100"
+
+  filter_criteria {
+    filter {
+      pattern = jsonencode({
+        "userIdentity" : {
+          "type" : ["Service"],
+          "principalId" : ["dynamodb.amazonaws.com"]
+        }
+      })
+    }
+  }
 }
